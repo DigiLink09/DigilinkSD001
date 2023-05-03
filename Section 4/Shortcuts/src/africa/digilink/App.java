cdd166116d1b2b1484d08a7fff4b5350874ba7c8
Nokuthula Mhlangu <nokuthula@harambee.co.za>
2023-05-03 15:41:40 +0200

    Section 4 & 5

---
+++ Section 4/Shortcuts/src/africa/digilink/App.java
@@ -0,0 +1,21 @@
+package africa.digilink;
+
+public class App {
+	
+	/*
+	 * F11 - Run app in debug mode
+	 * Ctr+F11 - Run app
+	 * Alt+Shift+R - Rename variables, class etc.
+	 * F3 - Jump to declaration
+	 * Ctr + D - Remove lines
+	 * Ctr+Space - Autocomplete 
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		
+		System.err.println("Error...");
+		
+		Temp temp;
+	}
+}
